3c7989be8f906461d687fa976012ffa50112d114
marioperator <40034568+marioperator@users.noreply.github.com>
2026-03-09 19:30:09 +0100

    Update nohello.js

--- it/nohello.js
+++ it/nohello.js
@@ -6,7 +6,7 @@ var typed2 = new Typed('#strike', {
     'Ciao, Solo un secondo...',
     'Ciao, domanda tanto ci metti 1 minuto..',
     'Sei online?',
-    'Ciao, ti vedo &#x26D4, posso disturbarti?',
+    'Ciao, ti vedo ⛔, posso disturbarti?',
     'Ciao, buondì',
     'Hai un attimo?',
     'Ci sei per caso?',
